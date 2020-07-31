c156e816b0fe6b5457e0289bfc0ecb25fe0886cb
Nick ODell <nickodell@gmail.com>
2020-07-31 12:38:56 -0600

    Also push base

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 set -euo pipefail
 docker build . -f Dockerfile.base -t nickodell/docker-github-actions-runner:base
 docker build . -f Dockerfile -t nickodell/docker-github-actions-runner:latest
+docker push nickodell/docker-github-actions-runner:base
 docker push nickodell/docker-github-actions-runner:latest
